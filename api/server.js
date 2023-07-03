543e0df01bcbf0e98c25b4c78a1cf1f1b80317e7
Thao Pham <thao.phamduy@asnet.com.vn>
2023-07-03 17:26:58 +0700

    update data json

--- api/server.js
+++ api/server.js
@@ -8,10 +8,10 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+    '/book/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(3000, () => {
+server.listen(8000, () => {
     console.log('JSON Server is running')
 })
 
